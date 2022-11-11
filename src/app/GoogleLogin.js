70986b84be7156d0ddb5efea897d63e997236787
AndresSY26 <107164709+AndresSY26@users.noreply.github.com>
2022-11-11 17:01:37 +0000

    version 1.0

---
+++ src/app/GoogleLogin.js
@@ -0,0 +1,22 @@
+import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
+import { showMessage } from './ShowMessage.js'
+import { auth } from './config.js'
+
+const GoogleBotton = document.querySelector("#LoginGoogle")
+
+GoogleBotton.addEventListener ('click', async () => {
+    const provider = new GoogleAuthProvider()
+
+    try {
+        const credentials = await signInWithPopup(auth, provider)
+
+        const Signin = document.querySelector('#SignIn')
+        const modal = bootstrap.Modal.getInstance(Signin)
+        modal.hide()
+
+        showMessage("Login correct " + credentials.user.displayName)
+
+    } catch (error) {
+        console.log(error)
+    }
+})
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.13.0/fi
 import { LoginCheck } from './app/LoginCheck.js'
 import { auth } from './app/config.js'
 
+import './app/GoogleLogin.js'
 import './app/SignupForm.js'
 import './app/SigninForm.js'
 import './app/logout.js'
