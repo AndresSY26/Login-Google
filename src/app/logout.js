85dc1d8b8a3615463897aad6d7782e5687ac9000
AndresSY26 <107164709+AndresSY26@users.noreply.github.com>
2022-11-12 09:14:10 -0500

    Delete src directory

--- src/app/logout.js
+++
@@ -1,9 +0,0 @@
-import { signOut } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js"
-import { auth } from './config.js'
-
-const Logout = document.querySelector("#Logout")
-
-Logout.addEventListener('click', async() => {
-    await signOut(auth)
-    console.log('user signed out')
-})
